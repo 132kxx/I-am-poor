232ee08a3ed74520762c7c7104b42c68d0b39e00
kxx <132kxx@gmail.com>
2022-10-03 19:02:23 +0900

    Initial Commit

---
+++ I am poor/I_am_poorApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_poorApp.swift
+//  I am poor
+//
+//  Created by kxx: on 2022/10/03.
+//
+
+import SwiftUI
+
+@main
+struct I_am_poorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
